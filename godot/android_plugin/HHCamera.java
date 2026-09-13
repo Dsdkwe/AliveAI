c98f75e09150c8b5218dd9d9fa3f5d16a403f800
DavaDtt <2123510996@qq.com>
2026-09-13 12:23:17 +0800

    fix(ar): 回调四重保险（缓冲顺序/实际格式/旧式回调/隐形预览面）+全程诊断记录

--- godot/android_plugin/HHCamera.java
+++ godot/android_plugin/HHCamera.java
@@ -6,7 +6,9 @@ import android.graphics.YuvImage;
 import android.hardware.Camera;
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.graphics.SurfaceTexture;
 import android.os.SystemClock;
+import android.view.Surface;
 
 import org.godotengine.godot.Godot;
 import org.godotengine.godot.plugin.GodotPlugin;
@@ -34,6 +36,8 @@ public class HHCamera extends GodotPlugin {
 	private volatile int cbCount = 0;
 	private volatile int jpgCount = 0;
 	private volatile String dbg = "init";
+	private SurfaceTexture dummySt = null;
+	private Surface dummySf = null;
 
 	public HHCamera(Godot godot) {
 		super(godot);
@@ -46,7 +50,7 @@ public class HHCamera extends GodotPlugin {
 
 	@UsedByGodot
 	public void start() {
-		dbg = "start_called";
+		dbg = "init";
 		synchronized (lock) {
 			if (camera != null) {
 				return;
@@ -68,148 +72,202 @@ public class HHCamera extends GodotPlugin {
 		});
 	}
 
-	private void openCamera() {
-		try {
-			int camId = -1;
-			Camera.CameraInfo info = new Camera.CameraInfo();
-			int n = Camera.getNumberOfCameras();
-			for (int i = 0; i < n; i++) {
-				Camera.getCameraInfo(i, info);
-				if (info.facing == Camera.CameraInfo.CAMERA_FACING_BACK) {
-					camId = i;
-					rotationDegrees = info.orientation;
-					break;
-				}
-			}
-			if (camId < 0) {
-				dbg = "no_cam n=" + n;
-				return;
-			}
-			dbg = "opening#" + camId;
-			final Camera c = Camera.open(camId);
-			if (c == null) {
-				dbg = "open_null";
-				return;
-			}
-			synchronized (lock) {
-				if (!wantActive) {
-					try {
-						c.release();
-					} catch (Throwable ignored) {
-					}
-					return;
-				}
-				camera = c;
-			}
-			Camera.Parameters params = c.getParameters();
-			List<Camera.Size> sizes = params.getSupportedPreviewSizes();
-			Camera.Size best = null;
-			for (Camera.Size s : sizes) {
-				if (s.width <= 1280) {
-					if (best == null || (long) s.width * s.height > (long) best.width * best.height) {
-						best = s;
-					}
-				}
-			}
-			if (best != null) {
-				params.setPreviewSize(best.width, best.height);
-			}
-			try {
-				List<Integer> fmts = params.getSupportedPreviewFormats();
-				if (fmts != null && fmts.contains(ImageFormat.NV21)) {
-					params.setPreviewFormat(ImageFormat.NV21);
-				} else {
-					dbg = "fmt_no_nv21";
-				}
-			} catch (Throwable t) {
-				dbg = "fmtE:" + t.getClass().getSimpleName();
-			}
-			String note = "";
-			boolean applied = false;
-			try {
-				c.setParameters(params);
-				applied = true;
-				note = "ok1";
-			} catch (Throwable t1) {
-				note = "1E:" + t1.getMessage();
-			}
-			if (!applied) {
-				try {
-					Camera.Parameters p2 = c.getParameters();
-					if (best != null) {
-						p2.setPreviewSize(best.width, best.height);
-					}
-					c.setParameters(p2);
-					applied = true;
-					note = note + "|ok2";
-				} catch (Throwable t2) {
-					note = note + "|2E:" + t2.getMessage();
-				}
-			}
-			if (!applied) {
-				try {
-					Camera.Parameters p3 = c.getParameters();
-					Camera.Size small = null;
-					for (Camera.Size s3 : p3.getSupportedPreviewSizes()) {
-						if (s3.width <= 640) {
-							if (small == null || (long) s3.width * s3.height > (long) small.width * small.height) {
-								small = s3;
-							}
-						}
-					}
-					if (small != null) {
-						p3.setPreviewSize(small.width, small.height);
-					}
-					c.setParameters(p3);
-					applied = true;
-					note = note + "|ok3";
-				} catch (Throwable t3) {
-					note = note + "|3E:" + t3.getMessage();
-				}
-			}
-			Camera.Size sz = c.getParameters().getPreviewSize();
-			final int fw = sz.width;
-			final int fh = sz.height;
-			dbg = "setp[" + note + "] " + fw + "x" + fh + " f=" + c.getParameters().getPreviewFormat();
-			int bufSize = fw * fh * ImageFormat.getBitsPerPixel(ImageFormat.NV21) / 8;
-			c.addCallbackBuffer(new byte[bufSize]);
-			c.addCallbackBuffer(new byte[bufSize]);
-			c.setPreviewCallbackWithBuffer(new Camera.PreviewCallback() {
-				@Override
-				public void onPreviewFrame(byte[] data, Camera cam) {
-					if (data == null) {
-						return;
-					}
-					try {
-						cbCount++;
-						long now = SystemClock.elapsedRealtime();
-						if (now - lastProcMs >= 33) {
-							lastProcMs = now;
-							YuvImage yuv = new YuvImage(data, ImageFormat.NV21, fw, fh, null);
-							ByteArrayOutputStream os = new ByteArrayOutputStream();
-							yuv.compressToJpeg(new Rect(0, 0, fw, fh), 60, os);
-							latestJpeg.set(os.toByteArray());
-							jpgCount++;
-							frameCount++;
-						}
-					} catch (Throwable t) {
-						dbg = "jpgE:" + t.getClass().getSimpleName();
-					}
-					try {
-						cam.addCallbackBuffer(data);
-					} catch (Throwable ignored) {
-					}
-				}
-			});
-			c.startPreview();
-			dbg = "started";
-		} catch (Throwable t) {
-			dbg = "E:" + t.getClass().getSimpleName() + ":" + t.getMessage();
-			synchronized (lock) {
-				releaseCamera();
+private void openCamera() {
+	try {
+		int camId = -1;
+		Camera.CameraInfo info = new Camera.CameraInfo();
+		int n = Camera.getNumberOfCameras();
+		for (int i = 0; i < n; i++) {
+			Camera.getCameraInfo(i, info);
+			if (info.facing == Camera.CameraInfo.CAMERA_FACING_BACK) {
+				camId = i;
+				rotationDegrees = info.orientation;
+				break;
 			}
 		}
+		if (camId < 0) {
+			dbg = dbg + "|no_cam n=" + n;
+			return;
+		}
+		dbg = dbg + "|opening#" + camId;
+		final Camera c = Camera.open(camId);
+		if (c == null) {
+			dbg = dbg + "|open_null";
+			return;
+		}
+		synchronized (lock) {
+			if (!wantActive) {
+				try {
+					c.release();
+				} catch (Throwable ignored) {
+				}
+				return;
+			}
+			camera = c;
+		}
+		Camera.Parameters params = c.getParameters();
+		List<Camera.Size> sizes = params.getSupportedPreviewSizes();
+		Camera.Size best = null;
+		for (Camera.Size s : sizes) {
+			if (s.width <= 1280) {
+				if (best == null || (long) s.width * s.height > (long) best.width * best.height) {
+					best = s;
+				}
+			}
+		}
+		if (best != null) {
+			params.setPreviewSize(best.width, best.height);
+		}
+		try {
+			List<Integer> fmts = params.getSupportedPreviewFormats();
+			if (fmts != null && fmts.contains(ImageFormat.NV21)) {
+				params.setPreviewFormat(ImageFormat.NV21);
+			} else {
+				dbg = dbg + "|fmt_no_nv21";
+			}
+		} catch (Throwable t) {
+			dbg = dbg + "|fmtE:" + t.getClass().getSimpleName();
+		}
+		String note = "";
+		boolean applied = false;
+		try {
+			c.setParameters(params);
+			applied = true;
+			note = "ok1";
+		} catch (Throwable t1) {
+			note = "1E:" + t1.getMessage();
+		}
+		if (!applied) {
+			try {
+				Camera.Parameters p2 = c.getParameters();
+				if (best != null) {
+					p2.setPreviewSize(best.width, best.height);
+				}
+				c.setParameters(p2);
+				applied = true;
+				note = note + "|ok2";
+			} catch (Throwable t2) {
+				note = note + "|2E:" + t2.getMessage();
+			}
+		}
+		if (!applied) {
+			try {
+				Camera.Parameters p3 = c.getParameters();
+				Camera.Size small = null;
+				for (Camera.Size s3 : p3.getSupportedPreviewSizes()) {
+					if (s3.width <= 640) {
+						if (small == null || (long) s3.width * s3.height > (long) small.width * small.height) {
+							small = s3;
+						}
+					}
+				}
+				if (small != null) {
+					p3.setPreviewSize(small.width, small.height);
+				}
+				c.setParameters(p3);
+				applied = true;
+				note = note + "|ok3";
+			} catch (Throwable t3) {
+				note = note + "|3E:" + t3.getMessage();
+			}
+		}
+		dbg = dbg + "|setp[" + note + "]";
+		Camera.Parameters pActual = c.getParameters();
+		Camera.Size sz = pActual.getPreviewSize();
+		final int fw = sz.width;
+		final int fh = sz.height;
+		int actualFmt = pActual.getPreviewFormat();
+		final int yuvFmt = (actualFmt == ImageFormat.NV21 || actualFmt == ImageFormat.YUY2) ? actualFmt : ImageFormat.NV21;
+		int bpp = ImageFormat.getBitsPerPixel(yuvFmt);
+		if (bpp <= 0) {
+			bpp = 12;
+		}
+		final int bufSize = fw * fh * bpp / 8;
+		dbg = dbg + "|sz " + fw + "x" + fh + " f=" + actualFmt + " bpp=" + bpp;
+		final Camera.PreviewCallback frameCb = new Camera.PreviewCallback() {
+			@Override
+			public void onPreviewFrame(byte[] data, Camera cam) {
+				if (data == null) {
+					return;
+				}
+				try {
+					cbCount++;
+					if (cbCount == 1) {
+						dbg = dbg + "|cb1";
+					}
+					long now = SystemClock.elapsedRealtime();
+					if (now - lastProcMs >= 33) {
+						lastProcMs = now;
+						YuvImage yuv = new YuvImage(data, yuvFmt, fw, fh, null);
+						ByteArrayOutputStream os = new ByteArrayOutputStream();
+						yuv.compressToJpeg(new Rect(0, 0, fw, fh), 60, os);
+						latestJpeg.set(os.toByteArray());
+						jpgCount++;
+						frameCount++;
+						if (jpgCount == 1) {
+							dbg = dbg + "|jpg1";
+						}
+					}
+				} catch (Throwable t) {
+					dbg = dbg + "|jpgE:" + t.getClass().getSimpleName();
+				}
+				try {
+					cam.addCallbackBuffer(data);
+				} catch (Throwable ignored) {
+				}
+			}
+		};
+		c.setPreviewCallbackWithBuffer(frameCb);
+		c.addCallbackBuffer(new byte[bufSize]);
+		c.addCallbackBuffer(new byte[bufSize]);
+		c.startPreview();
+		try {
+			c.addCallbackBuffer(new byte[bufSize]);
+		} catch (Throwable ignored) {
+		}
+		dbg = dbg + "|started";
+		camHandler.postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				if (camera == c && cbCount == 0 && wantActive) {
+					dbg = dbg + "|legacy_fb";
+					try {
+						c.setPreviewCallbackWithBuffer(null);
+						c.setPreviewCallback(frameCb);
+					} catch (Throwable t) {
+						dbg = dbg + "|legE:" + t.getClass().getSimpleName();
+					}
+				}
+			}
+		}, 4500);
+		camHandler.postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				if (camera == c && cbCount == 0 && wantActive) {
+					dbg = dbg + "|sf_fb";
+					try {
+						c.stopPreview();
+						dummySt = new SurfaceTexture(0);
+						dummySt.setDefaultBufferSize(fw, fh);
+						dummySf = new Surface(dummySt);
+						c.setPreviewDisplay(dummySf);
+						c.setPreviewCallbackWithBuffer(frameCb);
+						c.addCallbackBuffer(new byte[bufSize]);
+						c.addCallbackBuffer(new byte[bufSize]);
+						c.startPreview();
+					} catch (Throwable t) {
+						dbg = dbg + "|sfE:" + t.getClass().getSimpleName();
+					}
+				}
+			}
+		}, 7500);
+	} catch (Throwable t) {
+		dbg = dbg + "|E:" + t.getClass().getSimpleName() + ":" + t.getMessage();
+		synchronized (lock) {
+			releaseCamera();
+		}
 	}
+}
 
 	@UsedByGodot
 	public String getDebugInfo() {
